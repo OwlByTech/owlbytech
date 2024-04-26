4574e9baa56cccf54bd25d2cf19591a848703a16
camilo-camargo <camilocamargo49@gmail.com>
2024-04-26 17:35:36 -0500

    fix: height of all case studies

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -10,18 +10,22 @@ export default function ServicesCard() {
   const filteredServices = services.filter(data => data.languages_id === $defaultLanguage);
 
   return (
-  <div className="py-4 px-4 ">
-    <h1 className="text-text font-bold px-4 text-6xl">{item.services_title}</h1>
-    <p className="text-text px-4 text-2xl">{item.services_subtitle}</p>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 mx-auto max-w-screen-lg justify-center">
-      {filteredServices.map((data, index) => (
-        <div key={data.id} className="max-w-xl rounded-xl overflow-hidden shadow-lg border border-gray-800" style={{ transform: `translateY(${index * 100}px)` }}>
-          <h1 className="text-text font-bold text-3xl mb-5 mt-5">{data.title}</h1>
-          <p className="text-text">{data.description}</p>
-        </div>
-      ))}
+    <div className="py-4 px-4"
+      style={{
+        height: `${filteredServices.length * 200}px`
+      }}
+    >
+      <h1 className="text-text font-bold px-4 text-6xl">{item.services_title}</h1>
+      <p className="text-text px-4 text-2xl">{item.services_subtitle}</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 mx-auto max-w-screen-lg justify-center">
+        {filteredServices.map((data, index) => (
+          <div key={data.id} className="max-w-xl rounded-xl overflow-hidden shadow-lg border border-gray-800" style={{ transform: `translateY(${index * 100}px)` }}>
+            <h1 className="text-text font-bold text-3xl mb-5 mt-5">{data.title}</h1>
+            <p className="text-text">{data.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
 
 }  
