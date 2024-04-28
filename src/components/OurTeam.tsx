db05f40f17ec92f76d3abe031acce224a9b1dc98
camilo-camargo <camilocamargo49@gmail.com>
2024-04-28 01:49:57 -0500

    feat: our team space between each other.

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -6,13 +6,13 @@ export default function OurTeam() {
         <div className="flex flex-col mt-8 gap-12">
             <span className="text-3xl font-thin text-text">Our Team</span>
 
-            <div className="flex gap-4">
+            <div className="flex gap-4 justify-between">
             {
                 workers.map((data, index) => {
                     return (
                         <div key={index} className="flex flex-col w-36 h-36 justify-center gap-3">
                             <img src={`${directus.url}assets/${data.character}`}></img>
-                            <span className="text-text">{data.nickname}</span>
+                            <span className="text-center text-text">{data.nickname}</span>
                         </div>
                     );
                 })
