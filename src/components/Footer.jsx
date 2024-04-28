a4f5709526c0b1a0bd7c5c4f2dbce5b112bee3b3
Joseph <josephsilva0121@gmail.com>
2024-04-27 20:57:14 -0500

    fix container height

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,29 +2,34 @@ import { useStore } from "@nanostores/react";
 import { defaultLanguage } from "../services/LanguageStore";
 import { policies, socialMedia, technologies } from "../services/api";
 
-export default function Footer(){
+export default function Footer() {
     const $defaultLanguage = useStore(defaultLanguage);
     console.log(technologies);
     console.log(socialMedia);
     console.log(policies);
     const aboutUs = [
         {
-        name: "Open Source",
-        link: "#open-source"
-    },
-    {
-        name: "Code of Conduct",
-        link: "/code-of-conduct"
-    },
-    {
-        name: "Team",
-        link: "/about-us#team"
-    }
-];
+            name: "Open Source",
+            link: "#open-source"
+        },
+        {
+            name: "Code of Conduct",
+            link: "/code-of-conduct"
+        },
+        {
+            name: "Team",
+            link: "/about-us#team"
+        }
+    ];
 
-    return(
+    return (
         <div className="">
-            Hello World
+            {aboutUs.map(data => (
+                <div>
+                    <p className="text-text">{data.name}</p>
+                    <p>{data.link}</p>
+                </div>
+            ))}
         </div>
     );
 
