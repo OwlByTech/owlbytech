5e3d2caf83b5ffab0c0a58cb6edef94adf3f1857
camilo-camargo <camilocamargo49@gmail.com>
2024-04-28 23:34:17 -0500

    feat: add dockerization

---
+++ startup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# TODO: Check if the script is running on docker container or outside.
+echo "Installing bash..."
+apk add bash
+
+echo "Installing dependiencies"
+yarn install
+
+
+if [ $ENV = "prod" ]; then
+  echo "------------ PRODUCTION MODE ------------"
+  echo "Not implemented yet"
+else
+  echo "------------ DEVELOPMENT MODE ------------"
+  npm run dev -- --host
+fi
